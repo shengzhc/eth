2066cb5f57231315d244acf44883dc68e1217371
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-12-30 14:29:51 -0800

    update folder name

--- hello-world/scripts/deploy.js
+++ hello-world/scripts/deploy.js
@@ -1,7 +1,7 @@
 async function main() {
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
 
-    const hello_world = await HelloWorld.deploy("Hello WOrld!");
+    const hello_world = await HelloWorld.deploy("Hello World!");
     console.log("Contract deployed to address:", hello_world.address);
 }
 
