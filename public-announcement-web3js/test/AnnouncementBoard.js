22f3907872f184d4e6f11da2e4e0e2be986d1abf
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-12-31 23:13:33 -0800

    add web3js test for announcement board

--- public-announcement-web3js/test/AnnouncementBoard.js
+++ public-announcement-web3js/test/AnnouncementBoard.js
@@ -17,6 +17,24 @@ describe("AnnouncementBoard contract", function () {
   });
 
   describe("Contract announce & takedown", function () {
+    it("Announce should increase the number", async function () {
+      const board = await AnnouncementBoard.new(3);
+      await board.announce("1st Publishment");
+      assert.equal(await board.getAnnouncementsCount(), 1);
+    });
+
+    it("Announce multiple shouldn't exceed maximum", async function () {
+      const board = await AnnouncementBoard.new(3);
+      await board.announce("1st Publishment");
+      await board.announce("2nd Publishment");
+      await board.announce("3rd Publishment");
+      try {
+        await board.announce("4th Publishment");
+      } catch (_) {}
+      assert.equal(await board.getAnnouncementsCount(), 3);
+    });
     
+    it("Takedown should decrease the number", async function () {
+    });
   });
 });
