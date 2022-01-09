d12b62495d325e588173152999cd1a9b9eb968ba
Shengzhe Chen <shengzhe.chen@gmail.com>
2022-01-08 18:04:24 -0800

    add text field to capture announcement content

--- public-announcement-react/src/components/AnnounceBox.tsx
+++ public-announcement-react/src/components/AnnounceBox.tsx
@@ -1,25 +1,47 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { any } from 'prop-types';
 
-class AnnounceBox extends React.Component {
+class AnnounceBox extends React.Component<any, any> {
   render() {
     return (
       <Box
         component="form"
         sx={{
-          '& .MuiTextField-root': { m: 4, width: '480pt' },
+          '& .MuiTextField-root': { mt: 4, mb: 4, ml: '10%', maxWidth: '80%' },
         }}
         noValidate
         autoComplete="off">
-          <TextField 
-            required 
-            id="announce-content-text-input"
-            label="Announce something?"
-            defaultValue=""
-          />
+        <TextField
+          required
+          fullWidth
+          id="announce-content-text-input"
+          label="Announce something?"
+        />
+        <Button
+          variant="contained"
+          size="medium"
+          onClick={() => {this.announceBtnClicked();}}>
+          Announce
+        </Button>
       </Box>
     );
+  }
+
+  announceBtnClicked() {
+    const textField = 
+      document.getElementById('announce-content-text-input') as HTMLInputElement;
+    if (!textField) {
+      return;
+    }
+
+    const content = textField.value;
+    if (content.length < 1) {
+      alert("Please at least enter sth");
+      return;
+    }
   }
 }
 
